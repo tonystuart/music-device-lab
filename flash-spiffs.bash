3b6ee1eaf84b96f1c525f2eb6a6415c661aec1a1
Tony Stuart <anthonyfstuart@gmail.com>
2020-09-03 07:29:45 -0700

    Add soundfont to spiffs and restore original music.csv

--- flash-spiffs.bash
+++ flash-spiffs.bash
@@ -1,6 +1,6 @@
 set -x
 
-$IDF_PATH/components/spiffs/spiffsgen.py 1048576 spiffs/ spiffs.bin
+$IDF_PATH/components/spiffs/spiffsgen.py 1536000 spiffs/ spiffs.bin
 
 address=$(python $IDF_PATH/components/partition_table/gen_esp32part.py build/partitions.bin |
 	awk -F "," '/spiffs/ { print $4; }')
