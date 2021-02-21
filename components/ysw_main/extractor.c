ae9799de979d928a985ce5eb2ff9ca47cfb6c06f
Tony Stuart <anthonyfstuart@gmail.com>
2021-02-21 08:53:10 -0800

    Extract additional parameters

--- components/ysw_main/extractor.c
+++ components/ysw_main/extractor.c
@@ -16,8 +16,25 @@
 #include "stdio.h"
 #include "stdlib.h"
 
-static void extract_sample(char *ofn, FILE *csvf, fluid_sample_t *sample, int sx)
+// See http://www.synthfont.com/Tutorial6.html
+// See http://www.synthfont.com/SFSPEC21.PDF
+
+static const int iz_gen_x[] = {
+    GEN_FINETUNE,
+    GEN_OVERRIDEROOTKEY,
+};
+
+#define IZ_GEN_SZ (sizeof(iz_gen_x) / sizeof(iz_gen_x[0]))
+
+static void extract_sample(char *ofn, FILE *csvf, fluid_inst_zone_t *iz, int sx)
 {
+    fluid_sample_t *sample = fluid_inst_zone_get_sample(iz);
+    int length = sample->end - sample->start;
+    printf("keylo=%d, keyhi=%d, vello=%d, velhi=%d, name=%s, start=%d, end=%d, length=%d\n",
+            iz->range.keylo, iz->range.keyhi,
+            iz->range.vello, iz->range.velhi,
+            sample->name, sample->start, sample->end, length);
+
     int loop_start = (sample->loopstart - sample->start) / 2; // divide by 2 for int8_t
     int loop_end = (sample->loopend - sample->start) / 2;
     int loop_length = loop_end - loop_start - 1;
@@ -25,7 +42,18 @@ static void extract_sample(char *ofn, FILE *csvf, fluid_sample_t *sample, int sx
     char sn[64];
     ysw_csv_escape(sample->name, sn, sizeof(sn));
 
-    fprintf(csvf, "1,%d,%s,%d,%d,60,0\n", sx, sn, loop_start, loop_length);
+    fprintf(csvf, "1,%d,%s,%d,%d,60,0", sx, sn, loop_start, loop_length);
+
+    for (int i = 0; i < IZ_GEN_SZ; i++) {
+        fluid_gen_t *gen = &iz->gen[iz_gen_x[i]];
+        if (gen->flags) {
+            fprintf(csvf, ",%g", gen->val);
+        } else {
+            fprintf(csvf, ",");
+        }
+    }
+
+    fprintf(csvf, "\n");
 
     char sfn[128];
     snprintf(sfn, sizeof(sfn), "%s/samples/%s", ofn, sn);
@@ -52,7 +80,19 @@ static void extract_ranges(char *ofn, FILE *csvf, fluid_inst_t *inst, int *sx)
     }
 }
 
-static void extract_program(FILE *csvf, fluid_defpreset_t *dp, int px)
+static const int inst_gen_x[] = {
+    GEN_CHORUSSEND,
+    GEN_REVERBSEND,
+    GEN_VOLENVATTACK,
+    GEN_VOLENVDECAY,
+    GEN_VOLENVSUSTAIN,
+    GEN_VOLENVRELEASE,
+    GEN_ATTENUATION,
+};
+
+#define INST_GEN_SZ (sizeof(inst_gen_x) / sizeof(inst_gen_x[0]))
+
+static void extract_program(FILE *csvf, fluid_defpreset_t *dp, fluid_inst_t *inst, int px)
 {
     char preset_name[64];
     ysw_csv_escape(dp->name, preset_name, sizeof(preset_name));
@@ -60,21 +100,26 @@ static void extract_program(FILE *csvf, fluid_defpreset_t *dp, int px)
     while (length > 0 && preset_name[--length] == ' ') {
         preset_name[length] = 0;
     }
-    fprintf(csvf, "2,%d,%s,0,%d\n", px, preset_name, dp->num);
+
+    fprintf(csvf, "2,%d,%s,0,%d", px, preset_name, dp->num);
+
+    for (int i = 0; i < INST_GEN_SZ; i++) {
+        fluid_gen_t *gen = &inst->global_zone->gen[inst_gen_x[i]];
+        if (gen->flags) {
+            fprintf(csvf, ",%g", gen->val);
+        } else {
+            fprintf(csvf, ",");
+        }
+    }
+
+    fprintf(csvf, "\n");
 }
 
 static void extract_samples(char *ofn, FILE *csvf, fluid_inst_t *inst, int sx)
 {
     fluid_inst_zone_t *iz = fluid_inst_get_zone(inst);
     while (iz) {
-        fluid_sample_t *sample = fluid_inst_zone_get_sample(iz);
-        int length = sample->end - sample->start;
-        printf("keylo=%d, keyhi=%d, vello=%d, velhi=%d, name=%s, start=%d, end=%d, length=%d\n",
-                iz->range.keylo, iz->range.keyhi,
-                iz->range.vello, iz->range.velhi,
-                sample->name, sample->start, sample->end, length);
-
-        extract_sample(ofn, csvf, sample, sx);
+        extract_sample(ofn, csvf, iz, sx);
         iz = fluid_inst_zone_next(iz);
         sx++;
     }
@@ -82,9 +127,9 @@ static void extract_samples(char *ofn, FILE *csvf, fluid_inst_t *inst, int sx)
 
 static void write_comment(FILE *csvf)
 {
-    fprintf(csvf, "#type(1=sample),index,name,reppnt,replen,volume,pan\n");
-    fprintf(csvf, "#type(2=program),index,name,type,gm\n");
-    fprintf(csvf, "#type(3=patch),from-note,to-note,from-vel,to-vel,sample[,name]\n");
+    fprintf(csvf, "#type(1=sample),index,name,reppnt,replen,volume,pan,fine-tune,root-key\n");
+    fprintf(csvf, "#type(2=program),index,name,type,gm,chorus,reverb,attack,decay,sustain,release,attenuation\n");
+    fprintf(csvf, "#type(3=patch),from-note,to-note,from-vel,to-vel,sample\n");
 }
 
 static void extract_preset(char *ofn, FILE *csvf, fluid_preset_t *preset, int *px, int *sx)
@@ -97,7 +142,7 @@ static void extract_preset(char *ofn, FILE *csvf, fluid_preset_t *preset, int *p
         write_comment(csvf);
 
         extract_samples(ofn, csvf, inst, *sx);
-        extract_program(csvf, dp, *px);
+        extract_program(csvf, dp, inst, *px);
         extract_ranges(ofn, csvf, inst, sx);
 
         pz = fluid_preset_zone_next(pz);
